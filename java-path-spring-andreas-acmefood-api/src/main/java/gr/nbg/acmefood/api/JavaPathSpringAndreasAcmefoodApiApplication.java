1f8a2aceefde807573b52e4c338046343e5385f0
ΑΝΔΡΕΑΣ ΚΑΣΤΡΙΣΙΟΣ <KASTRISIOS.A@nbg.gr>
2023-09-28 09:21:43 +0300

    api module created

---
+++ java-path-spring-andreas-acmefood-api/src/main/java/gr/nbg/acmefood/api/JavaPathSpringAndreasAcmefoodApiApplication.java
@@ -0,0 +1,13 @@
+package gr.nbg.acmefood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPathSpringAndreasAcmefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPathSpringAndreasAcmefoodApiApplication.class, args);
+	}
+
+}
